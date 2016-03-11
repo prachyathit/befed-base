dab964504528c1b03b600387a1ed1b6c86d714da
meeser <prachyathit.k@gmail.com>
2016-03-11 14:46:00 +0000

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ User.create!( name: "Example User",
               password: "foobar",
               password_confirmation: "foobar",
               admin: true )
-              
+
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -12,4 +12,28 @@ User.create!( name: "Example User",
                 email: email,
                 password: password,
                 password_confirmation: password )
+end
+
+# restaurants
+10.times do |n|
+  name = Faker::Name.name
+  description = Faker::Lorem.sentence(5)
+  image_url = Faker::Internet.url
+  Restaurant.create!( name: name,
+                desc: description,
+                image_url: image_url )
+end
+
+# foods
+restaurants = Restaurant.order(:created_at).take(6)
+10.times do
+  name = Faker::Lorem.word
+  price = Faker::Number.number(2)
+  image_url = Faker::Internet.url
+
+  restaurants.each do |restaurant|
+    restaurant.foods.create!( name: name,
+                              price: price,
+                              image_url: image_url)
+  end
 end
