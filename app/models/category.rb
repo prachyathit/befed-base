73cb4ba225a9d7a1e83ad77e9a0c4fcca65aa87e
meeser <prachyathit.k@gmail.com>
2016-04-11 02:24:00 +0000

    scope test

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ActiveRecord::Base
   validates :image_url, presence: true
   has_many :restaurant_categories
   has_many :restaurants, through: :restaurant_categories
+  default_scope -> { order(:id) }
 end
