2a80a881e899a6be3def6f501f6f1ad10e8ac6e2
Alan <alanly211@gmail.com>
2016-05-07 17:28:31 -0700

    find self address using geocomplete

--- app/assets/javascripts/reg_maps.js
+++ app/assets/javascripts/reg_maps.js
@@ -14,13 +14,13 @@ $(document).ready(function() {
          //get the value of the result and put it where it is supposed to be
         $('.latitude').val(result.geometry.location.lat());
         $('.longitude').val(result.geometry.location.lng());
-        var map = new google.maps.Map(document.getElementById('map'));
+        var map = $(".location").geocomplete("map");
         map.setZoom(18);
         map.setCenter(result.geometry.location);
       });
 
       $(".location").bind("geocode:dragged", function(event, latLng){
-      var map = new google.maps.Map(document.getElementById('map'));
+      var map = $(".location").geocomplete("map");
         map.panTo(latLng);
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode({'latLng': latLng }, function(results, status) {
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
 
      $(".location-arrow-wrapper").click(function() {
-       var options = {
+       var timeoutOptions = {
          enableHighAccuracy: true,
          timeout: 5000,
          maximumAge: 0
@@ -52,37 +52,17 @@ $(document).ready(function() {
        $(".spinning-icon-wrapper").removeClass("hide");
        $('#map-container').slideDown();
        navigator.geolocation.getCurrentPosition(function(position){
-         var pos = {
-           lat: position.coords.latitude,
-           lng: position.coords.longitude
-         }
-         var map = new google.maps.Map(document.getElementById('map'));
-         marker = new google.maps.Marker({position: pos, title: 'BeFed Map', map: map});
-         map.setCenter(pos);
-         map.setZoom(17);
-         marker.setPosition(pos);
+         var map = $(".location").geocomplete("map");
+         google.maps.event.trigger(map, 'resize');
          $.ajax({
            url: "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude,
            success: function(result) {
              $(".location-arrow-wrapper").removeClass("hide");
              $(".spinning-icon-wrapper").addClass("hide");
-             $('.location').val(result.results[0].formatted_address);
+             $(".location").geocomplete("find", result.results[0].formatted_address);
            }
          })
-       }, null, options);
-     });
-
-     autocomplete = new google.maps.places.Autocomplete(document.getElementsByClassName("location")[0]);
-     autocomplete.addListener('place_changed', function() {
-       var place = autocomplete.getPlace();
-       $('.latitude').val(place.geometry.location.lat());
-       $('.longitude').val(place.geometry.location.lng());
-       var map = new google.maps.Map(document.getElementById('map'));
-       marker = new google.maps.Marker({position: place.geometry.location, title: 'BeFed Map', map: map});
-
-       map.setCenter(place.geometry.location);
-       map.setZoom(17);
-       marker.setPosition(place.geometry.location);
+       }, null, timeoutOptions);
      });
 
    });
