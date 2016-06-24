9ea86d99c827904a5d5c7b4adfe017030caa29f0
Prachyathit Kanburapa <prachyathit.k@gmail.com>
2016-06-24 13:34:55 +0000

    email3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
 
   def delivery_confirmation(user, cart, order)
+    attachments.inline['logo01.png'] = File.read('assets/images/logo01.png')
     @user = user
     @cart = cart
     @order = order
