b2dee32df9aad47d47e629f5eeac63433fcb73ba
Prachyathit Kanburapa <prachyathit.k@gmail.com>
2016-06-30 11:19:27 +0000

    from befed, header bug mobile

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -20,7 +20,7 @@ class UserMailer < ApplicationMailer
     @cart = cart
     @order = order
     subject_to_restaurant = "Befed Delivery Request Order number #{order.id}"
-    mail to: "saladfactory.befed@gmail.com", subject: subject_to_restaurant
+    mail to: "saladfactory.befed@gmail.com", subject: subject_to_restaurant, from: "befedtoday@gmail.com"
   end
 
   def password_reset(user)
