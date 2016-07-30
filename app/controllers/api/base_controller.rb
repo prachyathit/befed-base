b1a7a8d1ec0b781d383774b5acfaff48418bc654
Carryall <carryall369@gmail.com>
2016-07-30 11:18:05 +0700

    change how to authenticate user and get current user

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -4,14 +4,14 @@ module Api
 	  include ErrorHandler
 	  include	ErrorResponder
 
-
 		def authenticate_user!
-			access_token = request.headers['Authentication']
-			error401 unless current_user.present? and current_user.authenticated?(access_token)
+			error401 unless current_user.present?
 		end
 
 		def current_user
-			@current_user ||= User.where(id: request.headers['User-ID']).first
+			access_token = request.headers['Authentication']
+			user_id = request.headers['User-Id']
+			@current_user ||= User.where(id: user_id, access_token: access_token).first
 		end
 	end
 end
