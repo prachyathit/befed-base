1eb7c945ce75fd7aace60f20f28d6351fa9411a0
Carryall <carryall369@gmail.com>
2016-07-30 11:19:24 +0700

    finish up login and logout api

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,22 +1,27 @@
 module Api
 	class SessionsController < BaseController
 
-		def login
-			param! :email, 		String, required: true, format: email_regexp
+		before_action :authenticate_user!, only: [:destroy]
+
+		def create
+			param! :email, 		String, required: true, format: User::VALID_EMAIL_REGEX
       param! :password, String, required: true, min_length: 6
       
       user = User.where(email: params[:email]).first
       if user.present? and user.valid_password?(params[:password])
-      	response json: { user_id: user.id, access_token: user.access_token }
+      	render json: { user_id: user.id, access_token: user.generate_access_token }
       else
       	error401
       end
 		end
 
-		def logout
+		def destroy
+			if success = current_user.remove_access_token
+				render json: { success: success, message: 'Successfully Logged out'}
+			else
+				render json: { success: success, message: current_user.errors.full_messages }
+			end
 		end
-
-		def register
-		end
+		
 	end
 end
