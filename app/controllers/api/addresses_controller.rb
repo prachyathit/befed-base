67c7b48d206ce14cdeee5343934074c2ffe8c47b
Carryall <carryall369@gmail.com>
2016-09-07 21:10:19 +0700

    add create user address api

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -11,10 +11,37 @@ module Api
       render json: current_address
     end
 
+    def create
+      param! :name,           String, required: true
+      param! :is_default,     :boolean, default: false
+      param! :latitude,       Float, required: true
+      param! :longitude,      Float, required: true
+      param! :instruction,    String
+      param! :house_room_no,  String, required: true
+      param! :street,         String, required: true
+      param! :building_name,  String
+      param! :floor,          String
+      param! :province,       String
+      param! :postal_code,    String
+
+      address = current_user.addresses.new(address_params)
+      if address.save
+        render json: address
+      else
+        error422(address.errors.full_messages)
+      end
+    end
+
 		private
 
     def current_address
       @current_address ||= current_user.addresses.where(id: params[:id]).first
+    end
+
+    def address_params
+      params.permit( :name, :is_default, :latitude, :longitude, 
+        :instruction, :house_room_no, :street, :building_name, 
+        :floor, :province, :postal_code )
     end
 	end
 end
