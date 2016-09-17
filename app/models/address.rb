f997507b889a509a6a836959d9519a16df0b0e36
YooTanya <yootanya@gmail.com>
2016-09-17 17:54:26 +0700

    remove name validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,6 @@ class Address < ActiveRecord::Base
 	belongs_to :user
 
 	validates_presence_of :user, :latitude, :longitude
-	validates :name, uniqueness: true, if: "self.name.present?"
 	validates_uniqueness_of :is_default, :scope => :user_id, :unless => Proc.new { |address| not address.is_default }
 
 	before_create :mark_address_as_default, unless: "user.addresses.present?"
