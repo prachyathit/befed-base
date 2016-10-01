291542774fa9045c2a631892ded7b94f4f086e6c
Carryall <carryall369@gmail.com>
2016-10-01 14:40:04 +0700

    add more params for restaurant response

--- app/serializers/restaurant_array_serializer.rb
+++ app/serializers/restaurant_array_serializer.rb
@@ -1,3 +1,8 @@
 class RestaurantArraySerializer < ActiveModel::Serializer
-	attributes :id, :name, :desc, :image_url, :min_order
+	attributes :id, :name, :desc, :image_url, :min_order, :delivery_hours
+	attribute :dtime, key: :delivery_time
+
+	def delivery_hours
+		"11:00AM - 9:00PM"
+	end
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,10 @@
 class RestaurantSerializer < ActiveModel::Serializer
-	attributes :id, :name, :desc, :image_url, :min_order, :address, :latitude, :longitude
+	attributes :id, :name, :desc, :image_url, :min_order, :address, 
+		:latitude, :longitude, :delivery_hours
+	attribute :dtime, key: :delivery_time
+
+	def delivery_hours
+		"11:00AM - 9:00PM"
+	end
+
 end
