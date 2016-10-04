2b5acb32417d9f9e95c58f5c0358ceb35fe46b8a
Carryall <carryall369@gmail.com>
2016-10-04 21:25:31 +0700

    fix error when successfully paid

--- lib/modules/omise_gateway.rb
+++ lib/modules/omise_gateway.rb
@@ -10,6 +10,7 @@ module OmiseGateway
       })
       if charge.paid
         Rails.logger.info("Successfully paid by credit card")
+        charge
       else
         Rails.logger.error("Unsuccessfully paid by credit card: #{charge.failure_code} #{charge.inspect}")
         raise ArgumentError, "Credit Card info is incorrect."
