9a6f622452df29664b09dd04aa96e29ca7ed7bdb
Carryall <carryall369@gmail.com>
2016-11-15 23:37:49 +0700

    remove fill up address when address not complete on checkout page

--- app/models/address.rb
+++ app/models/address.rb
@@ -33,12 +33,4 @@ class Address < ActiveRecord::Base
 			self.province, self.postal_code
 		].compact.join(' ')
 	end
-
-	def need_more_detail?
-		not (house_room_no.present? or street.present? or 
-			building_name.present? or floor.present?) #or 
-			# subdistrict.present? or district.present? or 
-			# province.present? or postal_code.present? )
-	end
-
 end
