2e06b4195dc947a88a67d73a20c856d9b33ed098
Carryall <carryall369@gmail.com>
2017-01-09 00:29:08 +0700

    set config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
+
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+# Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+
+# require 'action_dispatch/middleware/session/dalli_store'
+Rails.application.config.session_store  ActionDispatch::Session::CacheStore, namespace: 'sessions', key: '_befed_app_session', expire_after: 1.day
