0e827b73619e5fc361b8e3c5cf7aef4a9c61a99e
Carryall <carryall369@gmail.com>
2017-01-10 21:19:35 +0700

    require dalli store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
   if ENV["MEMCACHEDCLOUD_SERVERS"]
     config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
-    config.session_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
   end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  # require 'action_dispatch/middleware/session/dalli_store'
+  require 'action_dispatch/middleware/session/dalli_store'
   Rails.application.config.session_store  ActionDispatch::Session::CacheStore, namespace: 'sessions', key: '_befed_app_session', expire_after: 1.day
 else
   Rails.application.config.session_store :cookie_store, key: '_befed_app_session'
