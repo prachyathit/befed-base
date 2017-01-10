e345401c8edad4778feea81ea647bfc1044516da
Carryall <carryall369@gmail.com>
2017-01-10 20:50:25 +0700

    use memcache on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 
 # Be sure to restart your server when you modify this file.
 
-# Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
-
-# require 'action_dispatch/middleware/session/dalli_store'
-Rails.application.config.session_store  ActionDispatch::Session::CacheStore, namespace: 'sessions', key: '_befed_app_session', expire_after: 1.day
+if Rails.env.production?
+  # require 'action_dispatch/middleware/session/dalli_store'
+  Rails.application.config.session_store  ActionDispatch::Session::CacheStore, namespace: 'sessions', key: '_befed_app_session', expire_after: 1.day
+else
+  Rails.application.config.session_store :cookie_store, key: '_befed_app_session'
+end
