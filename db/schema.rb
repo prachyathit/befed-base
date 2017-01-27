488bf89a434ab562f98e19d544e857432dcdf41d
Prachyathit Kanburapa <prachyathit.k@gmail.com>
2017-01-27 18:14:04 +0000

    update admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170101064338) do
+ActiveRecord::Schema.define(version: 20170127120231) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "name"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20170101064338) do
     t.integer  "service_fee_percent", default: 0
     t.float    "service_fee",         default: 0.0
     t.float    "sub_total"
+    t.string   "agent",               default: "Ray"
   end
 
   create_table "payments", force: :cascade do |t|
